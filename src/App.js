7839e096bb02709506dca929e1525b996e49789d
jgsamaaa <jamesgabrieldumaug12@gmail.com>
2022-11-10 20:36:09 -0700

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ContextProvider from './Context/ContextProvider';
+import About from './Pages/About/About';
+import AboutTwo from './Pages/About/AboutTwo';
+import BlogTwo from './Pages/Blog/BlogTwo';
+import ContactTwo from './Pages/Contact/ContactTwo';
+
 import HomeTwo from './Pages/Home/HomeTwo';
+import PortfiloTwo from './Pages/Portfilo/PortfiloTwo';
+import ResumeTwo from './Pages/Resume/ResumeTwo';
+import NotFound from './Share/NotFound';
 
 function App() {
+  useEffect(() => {
+    AOS.init({ duration: 1200 });
+    AOS.refresh();
+  }, []);
   return (
-    <div>
-      <HomeTwo />
-    </div>
+    <>
+      <ContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomeTwo />}>
+              <Route index element={<AboutTwo />} />
+              <Route path="about" element={<AboutTwo />} />
+              <Route path="resume" element={<ResumeTwo />} />
+              <Route path="contact" element={<ContactTwo />} />
+              <Route path="blogs" element={<BlogTwo />} />
+              <Route path="works" element={<PortfiloTwo />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </ContextProvider>
+    </>
   );
 }
 

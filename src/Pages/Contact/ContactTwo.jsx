d5e56f13def4aa127856340e9460005c98c819bf
jgsamaaa <jamesgabrieldumaug12@gmail.com>
2022-11-11 09:27:41 -0700

    Contact section done

--- src/Pages/Contact/ContactTwo.jsx
+++ src/Pages/Contact/ContactTwo.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Footer from '../../Share/Footer';
+import PageTitle from '../../Share/PageTitle';
+import CommonContact from './CommonContact';
 
 const ContactTwo = () => {
   return (
-    <div>
-      <h1>ContactTwo</h1>
-    </div>
+    <>
+      <PageTitle title="Contact"></PageTitle>
+      {/* End pagetitle */}
+
+      <div className="bg-white  lg:rounded-2xl dark:bg-[#111111]">
+        {/* Contact page title */}
+        <h2 className="after-effect after:left-60 after:top-[76px] mb-12 md:mb-[30px] pl-4 md:pl-[60px] pt-12">
+          Contact
+        </h2>
+        <CommonContact condition={true} />
+        <Footer bg={'#F8FBFB'} />
+      </div>
+    </>
   );
 };
 

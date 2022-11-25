7aaa045f7cb65b1318f78556e35a81e5029b764c
jgsamaaa <jamesgabrieldumaug12@gmail.com>
2022-11-25 15:36:07 -0700

    C:/Program Files/Git/fixing code

--- src/Pages/Portfilo/CommonPortfilo.jsx
+++ src/Pages/Portfilo/CommonPortfilo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Masonry from 'react-masonry-css';
 import Modal from 'react-modal';
 import UseData from '../../Hooks/UseData';

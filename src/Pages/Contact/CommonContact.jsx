e8c4f2f694406405533ee4687c72aa037747d515
jgsamaaa <jamesgabrieldumaug12@gmail.com>
2022-11-26 01:46:11 -0700

    C:/Program Files/Git/contact section - edited some text

--- src/Pages/Contact/CommonContact.jsx
+++ src/Pages/Contact/CommonContact.jsx
@@ -55,11 +55,11 @@ const CommonContact = ({ condition }) => {
     >
       <h3 className="text-4xl ">
         <span className="text-gray-lite dark:text-[#A6A6A6] ">
-          I'm always open to discussing product
+          I'm always open to talk about
         </span>
         <br />
         <span className="font-semibold dark:text-white">
-          design work or partnerships.
+          work and collaboration.
         </span>
       </h3>
 

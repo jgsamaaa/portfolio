d0ab4714c8fe2677d734a173bf456baaba3e0d93
gabriel <jamesgabrieldumaug12@gmail.com>
2023-02-27 01:36:06 -0700

    fix footer

--- src/Share/Footer.jsx
+++ src/Share/Footer.jsx
@@ -4,16 +4,7 @@ const Footer = () => {
   return (
     <footer className="overflow-hidden rounded-b-2xl">
       <p className="py-6 text-center text-gray-lite dark:text-color-910 ">
-        © 2023 All Rights Reserved by
-        <a
-          className="hover:text-[#FA5252] duration-300 transition"
-          href="https://themeforest.net/user/ib-themes"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-           James Gabriel Dumaug
-        </a>
-        .
+        © 2023 All Rights Reserved by James Gabriel Dumaug .
       </p>
     </footer>
   );
